e3c5363011a01ae48fb8c6d61baab6f54c062087
MinexAutomation <minex.automation@gmail.com>
2019-10-21 11:48:46 -0700

    Added light-weight immutable file-system entry class and file-system entry type enumeration.

---
+++ source/R5T.Gepidia.Base/Code/Classes/FileSystemEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace R5T.Gepidia
+{
+    public class FileSystemEntry
+    {
+        #region Static
+
+        public static FileSystemEntry New(string path, FileSystemEntryType type)
+        {
+            var entry = new FileSystemEntry(path, type);
+            return entry;
+        }
+
+        #endregion
+
+
+        public string Path { get; }
+        public FileSystemEntryType Type { get; }
+
+
+        public FileSystemEntry(string path, FileSystemEntryType type)
+        {
+            this.Path = path;
+            this.Type = type;
+        }
+    }
+}
